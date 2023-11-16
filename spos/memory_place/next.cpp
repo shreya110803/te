060e4b53fb6a8460e670bbf18f9c04b181973c8a
shreya110803 <149295419+shreya110803@users.noreply.github.com>
2023-11-17 01:20:02 +0530

    Update next.cpp

--- spos/memory_place/next.cpp
+++ spos/memory_place/next.cpp
@@ -1,139 +1,59 @@
-#include<iostream>
+// ---------------------Next Fit----------------------
+
+#include <bits/stdc++.h>
 using namespace std;
+
+void NextFit(int blockSize[], int m, int processSize[], int n)
+{	
+	int allocation[n], j = 0, t = m - 1;	
+	memset(allocation, -1, sizeof(allocation));
+	for(int i = 0; i < n; i++){
+		while (j < m){
+			if(blockSize[j] >= processSize[i]){
+				allocation[i] = j;
+				blockSize[j] -= processSize[i];
+				t = (j - 1) % m;
+				break;
+			}
+			if (t == j){
+			
+				t = (j - 1) % m;
+				break;
+			}
+			j = (j + 1) % m;
+		}
+	}
+	cout << "\nProcess No.\tProcess Size\tBlock no.\n";
+	for (int i = 0; i < n; i++) {
+		cout << " " << i + 1 << "\t\t\t\t" << processSize[i]
+			<< "\t\t\t\t";
+		if (allocation[i] != -1)
+			cout << allocation[i] + 1;
+		else
+			cout << "Not Allocated";
+		cout << endl;
+	}
+}
 int main()
 {
-	cout<<"\n\n......NEXT FIT......"<<endl;
-	cout<<"Enter total number of processes"<<endl;
-	int n;
-	cin>>n;
-    cout<<"enter total number of partitions"<<endl;
-    int pn;
-    cin>>pn;
-	string pr[n];
-	int size[n];
-	int block[n];
-	int partion[pn];
-	string fit[pn];
-	int alloc[pn];
-	int addr[pn+1];
-	float total=0;
-    float used=0;
-	addr[0]=0;
-	for(int i=0;i<n;i++)
-	{
-		block[i]=-1;
-	}
-	//cout<<"Enter memory partition"<<endl;
-	for(int i=0;i<pn;i++)
-	{
-		partion[i]=i+1;
-	}
-	
-	//cout<<"Enter memory partition"<endl;
-	for(int i=0;i<pn;i++)
-	{
-		fit[i]='E';
-	}
-
-    //cout<<"Enter memory partition size"<<endl;
-	for(int i=0;i<pn;i++)
-	{
-		cout<<"Enter partition size of partition["<<i+1<<"]: ";
-		cin>>alloc[i];	
-	}
-	
-	cout<<"Enter processes"<<endl;
-	for(int i=0;i<n;i++)
-	{
-		cout<<"Enter pr["<<i+1<<"]: ";
-		cin>>pr[i];	
-	}
-	
-	cout<<"Enter processes size"<<"k"<<endl;
-	for(int i=0;i<n;i++)
-	{
-		cout<<"Enter size of pr["<<i+1<<"]: ";
-		cin>>size[i];
-	}
-	
-	for(int i=1;i<pn+1;i++)
-	{
-		addr[i]=addr[i-1]+alloc[i-1];
-		total=addr[i];
-	}
-	
-    int p=0;
-    int select[pn];
-    int temp=0;
-    int temp1=0;
-    for(int j=0;j<pn;j++)
-    {
-        select[j]=0;
+	// int blockSize[] = { 5, 10, 20 };
+	// int processSize[] = { 10, 20, 5 };
+	int m, n;
+    cout << "Enter the number of blocks: ";
+    cin >> m;
+    int blockSize[m];
+    cout << "Enter the block sizes:\n";
+    for (int i = 0; i < m; i++) {
+        cin >> blockSize[i];
     }
-    
-	while(p<n)
-    {
-        for(int i=temp;i<pn;i++)
-        {
-            if(alloc[i]>=size[p] && select[i]!=1)
-            {
-                fit[i]=pr[p];
-				used=used+size[p];
-				block[p]=i;
-                temp=i+1;
-                select[i]=1;
-                break;
-            }
-            
-        }
-		if(temp>=pn)
-		{
-			temp=0;
-		}
-        p++;
+    cout << "Enter the number of processes: ";
+    cin >> n;
+    int processSize[n];
+    cout << "Enter the process sizes:\n";
+    for (int i = 0; i < n; i++) 
+	{
+        cin >> processSize[i];
     }
-    cout<<"processes"<<"\t"<<"size"<<"\t"<<"partition no."<<endl;
-	for(int i=0;i<n;i++)
-	{
-		cout<<pr[i]<<"\t\t"<<size[i]<<"k\t\t"<<block[i]<<endl;
-			
-	}cout<<endl;
-	cout<<"partition"<<"\t"<<"Allocation"<<"\t\t"<<"Address"<<endl;
-	int m=0;
-	while(m<pn+1)
-	{
-		for(int i=m;i<m+1;i++)
-		{
-            if(i<pn)
-            {
-                cout<<partion[i]<<"\t\t";
-            }
-            else{
-                cout<<"\t\t";
-            }
-			
-		}
-		for(int j=m;j<m+1;j++)
-		{
-            if(m<pn)
-            {
-                cout<<fit[j]<<"   ["<<alloc[j]<<"k]\t\t";
-            }
-            else{
-                cout<<"\t\t\t";
-            }
-			
-		}
-		for(int k=m;k<m+1;k++)
-		{
-			cout<<addr[k]<<"k";
-		}
-		cout<<endl;
-		m++;
-		
-	}
-	cout<<"\n memory used="<<used;
-    cout<<"\n total memory="<<total;
-    cout<<"\n Memory utlized= "<<used/total;
-	return 0;	
+    NextFit(blockSize, m, processSize, n);
+	return 0;
 }
